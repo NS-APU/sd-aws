576b4c4a88675c21b19b204155a27f7bbb5f639d
Mirenk <minikmwinsea@gmail.com>
2024-05-01 16:01:12 +0900

    [update] set some variable and output
    * add name_prefix variable
    * add route_table_1a/1c outputs

--- terraform/apps/babacafe/modules/networks/private_subnet/main.tf
+++ terraform/apps/babacafe/modules/networks/private_subnet/main.tf
@@ -1,37 +1,37 @@
-resource "aws_subnet" "app-1a" {
+resource "aws_subnet" "private-1a" {
   vpc_id = var.vpc_id
   cidr_block = var.cidr_block_1a
   availability_zone = var.availability_zone_1a
 
   tags = {
-    Name = "app-1a"
+    Name = "${var.name_prefix}-private-1a"
   }
 }
 
-resource "aws_subnet" "app-1c" {
+resource "aws_subnet" "private-1c" {
   vpc_id = var.vpc_id
   cidr_block = var.cidr_block_1c
   availability_zone = var.availability_zone_1c
 
   tags = {
-    Name = "app-1c"
+    Name = "${var.name_prefix}-private-1c"
   }
 }
 
-resource "aws_route_table" "app-1a" {
+resource "aws_route_table" "private-1a" {
   vpc_id = var.vpc_id
 }
 
-resource "aws_route_table" "app-1c" {
+resource "aws_route_table" "private-1c" {
   vpc_id = var.vpc_id
 }
 
-resource "aws_route_table_association" "app-1a" {
-  subnet_id = aws_subnet.app-1a.id
-  route_table_id = aws_route_table.app-1a.id
+resource "aws_route_table_association" "private-1a" {
+  subnet_id = aws_subnet.private-1a.id
+  route_table_id = aws_route_table.private-1a.id
 }
 
-resource "aws_route_table_association" "app-1c" {
-  subnet_id = aws_subnet.app-1c.id
-  route_table_id = aws_route_table.app-1c.id
+resource "aws_route_table_association" "private-1c" {
+  subnet_id = aws_subnet.private-1c.id
+  route_table_id = aws_route_table.private-1c.id
 }
