6c0c1450fd69058a96df95b67555e759641a898a
Mirenk <minikmwinsea@gmail.com>
2024-05-01 16:04:48 +0900

    [add] add production api records

--- terraform/apps/babacafe/modules/route53/main.tf
+++ terraform/apps/babacafe/modules/route53/main.tf
@@ -2,6 +2,10 @@ data "aws_route53_zone" "host" {
   name = var.domain_name
 }
 
+//
+// Production records
+//
+
 resource "aws_route53_zone" "babacafe-prod" {
   name = "babacafe.${var.domain_name}"
 }
@@ -30,6 +34,39 @@ resource "aws_route53_record" "babacafe-prod-ns" {
   ]
   ttl = 172800
 }
+
+resource "aws_route53_zone" "babacafe-prod-api" {
+  name = "api.babacafe.${var.domain_name}"
+}
+
+resource "aws_route53_record" "babacafe-prod-api" {
+  type = "A"
+  name = aws_route53_zone.babacafe-prod-api.name
+  zone_id = aws_route53_zone.babacafe-prod-api.zone_id
+
+  alias {
+    name = var.alb_dns_name
+    zone_id = var.alb_zone_id
+    evaluate_target_health = true
+  }
+}
+
+resource "aws_route53_record" "babacafe-prod-api-ns" {
+  type = "NS"
+  name = aws_route53_zone.babacafe-prod-api.name
+  zone_id = aws_route53_zone.babacafe-prod.zone_id
+  records = [
+    aws_route53_zone.babacafe-prod-api.name_servers[0],
+    aws_route53_zone.babacafe-prod-api.name_servers[1],
+    aws_route53_zone.babacafe-prod-api.name_servers[2],
+    aws_route53_zone.babacafe-prod-api.name_servers[3],
+  ]
+  ttl = 172800
+}
+
+//
+// Staging records
+//
 
 resource "aws_route53_zone" "babacafe-stag" {
   name = "babacafe-stag.${var.domain_name}"
