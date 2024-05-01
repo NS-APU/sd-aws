1310bcab5a6c0b565476100433b21f124da735a4
Mirenk <minikmwinsea@gmail.com>
2024-05-02 00:09:05 +0900

    [update] remove mistaken A records

--- terraform/apps/babacafe/modules/route53/main.tf
+++ terraform/apps/babacafe/modules/route53/main.tf
@@ -10,18 +10,6 @@ resource "aws_route53_zone" "babacafe-prod" {
   name = "babacafe.${var.domain_name}"
 }
 
-resource "aws_route53_record" "babacafe-prod" {
-  type = "A"
-  name = aws_route53_zone.babacafe-prod.name
-  zone_id = aws_route53_zone.babacafe-prod.zone_id
-
-  alias {
-    name = var.alb_dns_name
-    zone_id = var.alb_zone_id
-    evaluate_target_health = true
-  }
-}
-
 resource "aws_route53_record" "babacafe-prod-ns" {
   type = "NS"
   name = aws_route53_zone.babacafe-prod.name
